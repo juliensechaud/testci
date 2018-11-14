8976321a6c4d2c4616fc78927886d5efe62c7752
Julien Séchaud <julien.sechaud@happn.fr>
2018-11-14 16:04:53 +0100

    Initial Commit

---
+++ testci/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testci
+//
+//  Created by Julien Séchaud on 14/11/2018.
+//  Copyright © 2018 Julien Séchaud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
